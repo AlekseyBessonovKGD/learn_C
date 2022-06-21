2f899423850bd8b49484c9e7008d8ad822226d49
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-21 23:31:24 +0200

    Add IfElse Programm

---
+++ Example_005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter you name: ");
+string Name = Console.ReadLine();
+
+if(Name.ToLower() == "alexey")
+{
+Console.Write("Ура, привет дружище!!!");
+}
+else
+{
+Console.Write("Добрый день, пользователь ");
+Console.Write(Name);
+}
