b8362ec484630b5556e55ed9c90b60dab76438fc
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-22 15:50:16 +0200

    add friends and dog programm

---
+++ Example_008_Friends/Program.cs
@@ -0,0 +1,24 @@
+﻿int count = 0;
+double distance = 100000;
+int speed_friend_1 = 2;
+int speed_friend_2 = 7;
+int speed_dog = 10;
+int friend = 1;
+double time = 0;
+
+while (distance > 10)
+{
+    if (friend % 2 == 0)
+    {
+        time = distance / (speed_friend_1 + speed_dog);
+    }
+    if (friend % 2 > 0)
+    {
+        time = distance / (speed_friend_2 + speed_dog);
+    }
+    distance = distance - (speed_friend_1 + speed_friend_2) * time;
+    friend = friend +1;
+    count = count + 1;
+}
+Console.Write("количество раз, которое пробежала собака равно ");
+Console.WriteLine(count);
