60737483c7e17080a526b48760e72e9ff25ab106
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-23 21:09:50 +0200

    поиск последнего числа в трехзначном числе

---
+++ Task_004/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное чило ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = number % 10;
+Console.WriteLine("последнее цифра трехзначного числа равно ");
+Console.WriteLine(number2);
