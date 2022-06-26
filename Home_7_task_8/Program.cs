c7c30ce73eadda4c9a29513e16e7da73883e21ce
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-26 10:29:24 +0200

    изменил программу 8дз

--- Home_7_task_8/Program.cs
+++ Home_7_task_8/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("введите число, будут показаны все четные числа в интервале от 1 до этого числа");
 int number = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 2; i < number; i = i + 2)
+for (int i = 2; i <= number; i = i + 2)
 {
     Console.Write($"{i},"); 
 }
