a24bbcbe721b0b7e9caeb24d0669953c8c8ef55f
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-26 09:41:06 +0200

    Домашнее задание 6

---
+++ Home_7_Task_6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число для проверки его четности");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 > 0){
+    Console.WriteLine("введенное число нечетное");
+}
+else{
+    Console.WriteLine("введенное число четное");
+}
