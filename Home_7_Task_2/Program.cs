38d919706296f4d9cef8d512adcf88b9ec66ac26
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-26 09:19:09 +0200

    домашнее задание 2

---
+++ Home_7_Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+int number_b = Convert.ToInt32(Console.ReadLine());
+
+if(number_a > number_b)
+    {
+        Console.WriteLine($"большее число равно {number_a} , меньшее равно {number_b}");
+    }
+else{
+        Console.WriteLine($"большее число равно {number_b} , меньшее равно {number_a}");
+}
