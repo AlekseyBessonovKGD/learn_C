9ff4a012f7c531fe3ddf16cd1657fcbf88c29a02
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-27 22:20:11 +0200

    Задача проверкиявляется ли одно чисо квадратом другого

---
+++ Lesson_8_Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите два числа ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+int number_b = Convert.ToInt32(Console.ReadLine());
+if ( number_a * number_a == number_b)
+{
+    Console.WriteLine("да");
+}
+else if ( number_b * number_b == number_a)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
