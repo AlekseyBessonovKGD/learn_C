5cf30805e1e89fe3dedc7f61506b1199e331710a
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-27 21:00:37 +0200

    Задача с выводом двух крайних чисел случайного трехзначного числа

---
+++ Lesson_8_Task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100, 999);
+Console.WriteLine(number);
+int first_number = (number / 100) * 10;
+int third_number = number % 10;
+int final_number = first_number + third_number;
+Console.WriteLine(final_number);
