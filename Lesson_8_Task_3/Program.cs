3e07a28f20b91f08a394691a5c805b719661032b
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-27 21:53:18 +0200

    Задача на двойную проверку кратности

---
+++ Lesson_8_Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("введите число для проверки его кратности одновременно 7 и 23");
+int first_number = Convert.ToInt32(Console.ReadLine());
+//int second_number = first_number;
+
+if(first_number % 7 == 0)
+{
+    if(first_number % 23 == 0)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+else 
+{
+    Console.WriteLine("нет");
+}
