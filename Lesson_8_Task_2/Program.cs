673bba1cd03ed52368d7cce7290737f2a52cb7ee
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-27 21:33:47 +0200

    Добавили программу для проверки кратности чисел

---
+++ Lesson_8_Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите два числа для проверки кратности");
+int first_number = Convert.ToInt32(Console.ReadLine());
+int second_number = Convert.ToInt32(Console.ReadLine());
+int result = (first_number % second_number);
+if(first_number % second_number > 0)
+{
+    Console.WriteLine($"не кратно, остаток от деления {result}");
+}
+else 
+{
+    Console.WriteLine("первое число кратно второму");
+}
