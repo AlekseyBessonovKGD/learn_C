6253b0da2c01568faa6f56c421aabbf720ce5955
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-27 22:42:42 +0200

    Дз 8 семинар, задача про нахождение второго числа в трехзначном числе

---
+++ Home_8_Task_10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >= 100 && number <=999)
+{
+    int result = ((number - (number % 10)) % 100) / 10;
+    Console.WriteLine($"вторая цифра числа равна {result}");
+}
+else if(number <= -100 && number >= -999)
+{
+    int result = (((number - (number % 10)) % 100) / 10) * -1;
+    Console.WriteLine($"вторая цифра числа равна {result}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число, попробуйте снова");
+}
