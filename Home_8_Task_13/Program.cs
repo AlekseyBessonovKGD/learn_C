d3eb20355e7e52b4a122ca32c89876d61f355840
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-29 00:22:09 +0200

    Дз Семинар 8 задача про отображение третьей цифры числа

---
+++ Home_8_Task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int any_number = Convert.ToInt32(Console.ReadLine());
+any_number = Math.Abs(any_number);
+if (any_number < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    int convert_to_three_digit_number = any_number;
+    int third_digit_of_number = 0;
+    for (int i = 1; convert_to_three_digit_number > 999; i = i + 1)
+    {
+        convert_to_three_digit_number = convert_to_three_digit_number / 10;
+    }
+    third_digit_of_number = convert_to_three_digit_number % 10;
+    Console.WriteLine($"третья цифра числа равна {third_digit_of_number}");
+}
