8fd45d9c0e3c8c23d802c9c9a4c56beda76b01ac
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-06-29 00:49:18 +0200

    Дз 8 семинар, задача про дни недели

---
+++ Home_8_Task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите цифру соответствующую дню недели");
+int day_of_week = Convert.ToInt32(Console.ReadLine());
+if (day_of_week < 1 || day_of_week > 7)
+{
+    System.Console.WriteLine("Введенное число не соответствует ни одному дню недели попробуйте снова");
+}
+else if (day_of_week >= 1 && day_of_week <= 5)
+{
+    System.Console.WriteLine("нет, это будний день");
+}
+else
+{
+    System.Console.WriteLine("да, это выходной день");
+}
