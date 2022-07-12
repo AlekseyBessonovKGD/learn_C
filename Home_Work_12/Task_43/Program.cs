f5523c1c6a69e6361452da7ea702ac930c84c839
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-07-12 23:21:53 +0200

    Дз к семинару 12

---
+++ Home_Work_12/Task_43/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.WriteLine("задайте значение переменной к1 в уравнении у = k1*x + b1");
+double number_k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("задайте значение переменной b1 в уравнении у = k1*x + b1");
+double number_b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("задайте значение переменной k2 в уравнении у = k2*x + b2");
+double number_k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("задайте значение переменной b2 в уравнении у = k2*x + b2");
+double number_b2 = Convert.ToDouble(Console.ReadLine());
+
+double cross_point_x = (number_b2 - number_b1) / (number_k1 - number_k2);
+double cross_point_y = (number_b1*number_k2 - number_b2*number_k1) / (number_k2 - number_k1);
+
+Console.WriteLine($"Прямые пересекутся в точке с координатами ({Math.Round(cross_point_x, 1)}; {Math.Round(cross_point_y, 1)})");
---
+++ Home_Work_12/task_41/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.WriteLine("Введите последовательно несколько чисел через запятую");
+string text_number = Console.ReadLine();
+
+string converted_text(string text, char old_char, string Empty) 
+{
+    string remoove_simbols_from_text = string.Empty;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if(text[i] == old_char) 
+        {
+            remoove_simbols_from_text = remoove_simbols_from_text + $"{Empty}";
+        }
+        else
+          remoove_simbols_from_text = remoove_simbols_from_text + $"{text[i]}";
+    }
+    return remoove_simbols_from_text;
+}
+
+int count_of_positive_numbers(int[] digital_array)
+{
+    int count = 0;
+    for (int i = 0; i < digital_array.Length; i++)
+    {
+        if (digital_array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+string text_without_spaces = converted_text(text_number, ' ', string.Empty);
+int[] digital_array = text_without_spaces.Split(',').Select(int.Parse).ToArray();
+int count_of_plus_numbers = count_of_positive_numbers(digital_array);
+
+if (count_of_plus_numbers == 1)
+{
+    Console.WriteLine($"вы ввели {count_of_plus_numbers} число больше 0");
+}
+else if (count_of_plus_numbers > 1 && count_of_plus_numbers < 5)
+{
+    Console.WriteLine($"вы ввели {count_of_plus_numbers} числа больше 0");
+}
+else if (count_of_plus_numbers == 0 || count_of_plus_numbers > 4)
+{
+    Console.WriteLine($"вы ввели {count_of_plus_numbers} чисел больше 0");
+}
